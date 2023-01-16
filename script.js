6fcdb6051e9eb0704731415213b056f84cc41143
Kornel Migdalek <kornel.migdalek@gmail.com>
2023-01-16 22:05:26 +0100

    end preload game page

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ setTimeout(() => {
 
 
 
- const spnText = document.querySelector('.text');
- const spnCursor = document.querySelector('.cursor');
+ const spanText = document.querySelector('.text__text');
+ const spanCursor = document.querySelector('.text__cursor');
  const txt = ['tekst1', 'tekst2', 'tekst3']
 
  let txtNumber = 0
@@ -33,24 +33,54 @@ setTimeout(() => {
  const addLetter = () => {
 
     if((txtLetter >= txt[txtNumber].length )) {
-        console.log('chuj');
-        txtLetter = -2
+        txtLetter = -20
         txtNumber++
         if(txtNumber < txt.length) {
-        spnText.textContent = ''
-     } else clearInterval(writingInterval)
-        
+        setTimeout(() => {
+            spanText.textContent = ''    
+        }, 2000); 
+     }  else { 
+        for (let i = 1; i < 100; i++) {
+            window.clearInterval(i);
+          }
+          spanCursor.classList.remove('active');
+            setTimeout(deleteText, 2000)
+     }
     } 
-
-    
     if((txtLetter >= 0) && (txtNumber < txt.length) ) {
-spnText.textContent += txt[txtNumber][txtLetter]
+spanText.textContent += txt[txtNumber][txtLetter]
     } 
-  
 txtLetter++
-
 }
+// Mechanizm działania kursora
 const cursorAnimation = () => {
-    spnCursor.classList.toggle('active');
+    spanCursor.classList.toggle('active');
    }
-   
+
+
+
+const timer = document.querySelector('.timer')
+
+// Referencja w funkcji addLetter()
+const deleteText = () => {
+    document.querySelector('.text').remove()
+    timer.classList.add('active')
+    // setTimeout(timerCounting, 1000)  
+    setInterval(timerCounting, 1000)
+}
+
+
+const timeSpan = document.querySelector('.timer__counting');
+let timeToGame = 3
+
+const timerCounting = () => {
+    if(timeToGame > 0) {
+    timeToGame--
+    timeSpan.textContent = timeToGame
+} else { 
+    for (let i = 1; i < 1000000; i++) {
+        window.clearInterval(i);
+        timer.remove()
+      }
+}
+}
